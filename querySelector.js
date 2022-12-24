8a9c9f3b177bed788f3e63d8314347d5c6bb50a2
25061997 <121218252+25061997@users.noreply.github.com>
2022-12-24 12:42:03 +0530

    querySelector

---
+++ querySelector.js
@@ -0,0 +1,11 @@
+let secondItem=document.querySelector(".list-group-item:nth-child(2)")
+secondItem.style.backgroundColor="green"
+let thirdItem=document.querySelector(".list-group-item:nth-child(3")
+thirdItem.style.color="white"
+
+let Item=document.querySelectorAll(".list-group-item")
+item[1].style.color=("green")
+let odd = document.querySelectorAll(".li:nth-child(odd")
+for(i=o;i<odd.length;i++){
+    odd[i].style.backgroundColor="green"
+}
