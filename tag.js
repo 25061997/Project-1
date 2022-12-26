4077287672206f75e1ea1ec0950555325c68f6da
25061997 <121218252+25061997@users.noreply.github.com>
2022-12-26 14:14:45 +0530

    Delete tag.js

--- tag.js
+++
@@ -1,7 +0,0 @@
-function saveData(){
-
-let Name=document.getElementById('name').value;
-let Email=document.getElementById('email').value;
-localStorage.setItem('name',Name)
-localStorage.setItem('email',Email)
-}
